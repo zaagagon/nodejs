59451d6e067831fd3b35a3932a29d51ff4006a0c
System Administrator <zaagagon@gmail.com>
2021-12-04 23:05:12 -0500

    ejecutando el servidor

--- servidor.js
+++ servidor.js
@@ -1,4 +1,16 @@
+var http = require('http');
+var port = 3000;
+
 http.createServer (function(req,res){
 
     res.writeHead(200,{'Content-type':'text/html'});
-});
+    res.write('<h1>Hola Zag</h1>');
+    res.end();
+}).listen(port);//podemos crear un mensaje de conexion exitosa 
+//usemos console log
+//en el caso de error por puerto ocupado
+//usemos el comando kill
+/* 
+$ lsof -i tcp:3000
+$ kill -9 PID */
+console.log("Servidor corriendo en el puerto"+port);
