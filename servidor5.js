886705ac23e85a99bdc46082cb12c9e2fd74f01e
System Administrator <zaagagon@gmail.com>
2021-12-06 09:11:34 -0500

    servidor con comentarios

---
+++ servidor5.js
@@ -0,0 +1,10 @@
+//elementos del servidor
+//puerto objeto http
+
+var http = require('http');
+const { createServer } = require('https');
+
+//objeto http para llamar el modulo createServer
+
+// Estructura de mi servidor http.createServer(function(){});
+//funcion callback
