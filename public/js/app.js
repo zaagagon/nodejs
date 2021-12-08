eb7ec1e55fed049b0a018afddc0414e2858f3819
System Administrator <zaagagon@gmail.com>
2021-12-07 23:41:12 -0500

    script funcionando

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,9 @@
-document.getElementById('title')
+//funcion timer
+/*setTimeout(()=>{
+
+})*/
+
+setTimeout(()=>{
+    document.getElementById('title')
     .innerHTML='Texto modificado por javascript en etiqueta h1'
+},3000);//4 segundos
