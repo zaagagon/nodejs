67f0c4cf4e0f753a13cbb097c17b2fe6ad11662d
System Administrator <zaagagon@gmail.com>
2021-12-08 15:38:09 -0500

    error solucionado

---
+++ servidor/app.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+//creamos el servidor 
+
+app.listen(3000,()=>{
+   console.log('hola servidor en puerto 3000')});//probemos el servidor
+//console.log("servidor express ok");
+
+
+//archivo index.js
+//archivo index.js
+/*const express = require('express');
+const app = express();
+
+const PUERTO = 3000;
+
+app.listen(PUERTO, function(){
+	console.log('Servidor http corriendo en el puerto 80');
+});*/
