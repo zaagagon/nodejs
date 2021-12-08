31269b99fc0282db55f488f3b088fd16860ed784
System Administrator <zaagagon@gmail.com>
2021-12-07 23:48:23 -0500

    document writeln

--- public/js/app.js
+++ public/js/app.js
@@ -6,5 +6,7 @@
 setTimeout(()=>{
     document.getElementById('title')
     .innerHTML='Texto modificado por javascript en etiqueta h1'
+    //document.writeln("probrando script");
 },4000);//4 segundos
 //repasar otras funciones
+document.writeln("script ok");//escribimos directamente en el html
