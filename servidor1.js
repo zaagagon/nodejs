436b19965bfb84263f5f80979623ff8190b57a45
System Administrator <zaagagon@gmail.com>
2022-03-23 00:07:21 -0500

    server1 express

--- servidor1.js
+++ servidor1.js
@@ -7,12 +7,13 @@ http.createServer (function(req,res){
 
    res.writeHead(200,{'Content-type':'text/html'});
    res.write('<h1>Hola Zag servidor corriendo con Nodemon</h1>');
-    res.end("Final de la prueba");
-    url=urls.parse(req.url).pathname;
-    router(rutas,url, res);//le pasamos 3 objetos
+    res.end("Final de la prueba Marzo");
+    res.end("Script con write");
+    //url=urls.parse(req.url).pathname;
+    //router(rutas,url, res);//le pasamos 3 objetos
 
    // console.log(url);
     
-}).listen(1000,'localhost',function(){
-    console.log("servidor corriendo en el puerto : "+ port);
+}).listen(1000,'localhost',function(){//1000 puede ser remplazado por la variable port
+    console.log("servidor corriendo en el puerto : "+ 1000);
 });
