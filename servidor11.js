69dc9fac2fb13df2ff4379b3f7ea95aec815f2d5
System Administrator <zaagagon@gmail.com>
2022-03-24 17:47:22 -0500

    res.end

--- servidor11.js
+++ servidor11.js
@@ -8,6 +8,6 @@ const servidor = http.createServer(function(req,res){
     //podemos usar res.write para escribir codigo html
     res.write('<h1>Titulo del servidor : Amadeus</h1>');
     res.write('<h2>**Servidor Olimpo **</h2>');
-    res.end("Hola zag servidor ok");
+    res.end("Hola zag servidor ok");//solo se puede llamar una vez
 
 });servidor.listen(9000);//puerto 9000
