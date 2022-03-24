4bebbe311c2ae6a2827d204e0a2d717406188970
System Administrator <zaagagon@gmail.com>
2022-03-23 23:24:01 -0500

    servidor.listen(9000)

--- servidor11.js
+++ servidor11.js
@@ -7,4 +7,4 @@ const servidor = http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/plain'});
     res.end("Hola zag servidor ok");
 
-});servidor.listen(9000);
+});servidor.listen(9000);//puerto 9000
