90b8a14bf3973aaa9b170eb7e76b3f7baba1e8cc
System Administrator <zaagagon@gmail.com>
2022-03-24 18:02:49 -0500

    res.write p

--- servidor11.js
+++ servidor11.js
@@ -2,12 +2,14 @@
 // server.js
 'use strict';
 const http = require('http');
+const { resourceLimits } = require('worker_threads');
 
 const servidor = http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/html'});//text/plain
     //podemos usar res.write para escribir codigo html
     res.write('<h1>Titulo del servidor : Amadeus</h1>');
     res.write('<h2>**Servidor Olimpo **</h2>');
+    res.write('<p>Servidor creado en express con node js</p>')
     res.end("Hola zag servidor ok");//solo se puede llamar una vez
 
 });servidor.listen(9000);//puerto 9000
