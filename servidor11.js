b493d6aeff5c0d7a07120a5f98ffa2a8955ea5b7
System Administrator <zaagagon@gmail.com>
2022-03-24 17:44:24 -0500

    server olimpo

--- servidor11.js
+++ servidor11.js
@@ -7,6 +7,7 @@ const servidor = http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/html'});//text/plain
     //podemos usar res.write para escribir codigo html
     res.write('<h1>Titulo del servidor : Amadeus</h1>');
+    res.write('<h2>**Servidor Olimpo **</h2>');
     res.end("Hola zag servidor ok");
 
 });servidor.listen(9000);//puerto 9000
