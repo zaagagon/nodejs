a076813110fbb4d945de7d7e063eb9d6d2906c90
System Administrator <zaagagon@gmail.com>
2022-03-23 23:27:55 -0500

    text/plain - text/html

--- servidor11.js
+++ servidor11.js
@@ -4,7 +4,9 @@
 const http = require('http');
 
 const servidor = http.createServer(function(req,res){
-    res.writeHead(200,{'content-type':'text/plain'});
+    res.writeHead(200,{'content-type':'text/html'});//text/plain
+    //podemos usar res.write para escribir codigo html
+    res.write('<h1>Titulo del servidor : Amadeus</h1>');
     res.end("Hola zag servidor ok");
 
 });servidor.listen(9000);//puerto 9000
