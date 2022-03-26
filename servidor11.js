4e7b00f40bc9c65f4d12bafa8fae9160fbb8743d
System Administrator <zaagagon@gmail.com>
2022-03-25 23:04:27 -0500

    style=color:red

--- servidor11.js
+++ servidor11.js
@@ -8,9 +8,9 @@ const servidor = http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/html'});//text/plain
     //podemos usar res.write para escribir codigo html
     res.write('<h1>Titulo del servidor : Amadeus</h1>');
-    res.write('<h2>**Servidor Olimpo **</h2>');
+    res.write('<h2 style= color:red>**Servidor Olimpo **</h2>');//color atributo
     res.write('<p>Servidor creado en express con node js</p>');
-    res.write();
+    res.write('');
     res.end("Hola zag servidor ok");//solo se puede llamar una vez
 
 });servidor.listen(9000);//puerto 9000
