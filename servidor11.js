76b2376f147b27ca9d4b256ac67729f3cbb042be
System Administrator <zaagagon@gmail.com>
2022-03-25 23:05:16 -0500

    h3

--- servidor11.js
+++ servidor11.js
@@ -10,7 +10,7 @@ const servidor = http.createServer(function(req,res){
     res.write('<h1>Titulo del servidor : Amadeus</h1>');
     res.write('<h2 style= color:red>**Servidor Olimpo **</h2>');//color atributo
     res.write('<p>Servidor creado en express con node js</p>');
-    res.write('');
+    res.write('<h3><n3/>');
     res.end("Hola zag servidor ok");//solo se puede llamar una vez
 
 });servidor.listen(9000);//puerto 9000
