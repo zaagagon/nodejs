c973019bd3ba7f998286d050a15f89048165fe7a
System Administrator <zaagagon@gmail.com>
2022-03-25 23:01:04 -0500

    cambios

--- servidor11.js
+++ servidor11.js
@@ -9,7 +9,8 @@ const servidor = http.createServer(function(req,res){
     //podemos usar res.write para escribir codigo html
     res.write('<h1>Titulo del servidor : Amadeus</h1>');
     res.write('<h2>**Servidor Olimpo **</h2>');
-    res.write('<p>Servidor creado en express con node js</p>')
+    res.write('<p>Servidor creado en express con node js</p>');
+    res.write();
     res.end("Hola zag servidor ok");//solo se puede llamar una vez
 
 });servidor.listen(9000);//puerto 9000
