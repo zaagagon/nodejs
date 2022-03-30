52dd313caa500ff9394068d1cc01ece8e0c255cb
System Administrator <zaagagon@gmail.com>
2022-03-29 23:35:47 -0500

    cambios

--- expreso.js
+++ expreso.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 30003
+const port = 30004
 
 app.get('/', (req, res) => {
    // res.write('<h1>Express funcionando</h1>');
-  res.send('<h1 style= color: blue>Hello zag!</h1>')
+   res.writeHead(200,{'Content-type':'text/html'});//para que funcione necesita
+    res.write('<h1>Pagina principal ADMIN</h1>');
+  res.send('<h1 style= "color: blue">Hello zag!</h1>')//color tag
 })
 
 app.listen(port, () => {
