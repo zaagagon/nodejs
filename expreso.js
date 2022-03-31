01460bf32cba31228594ae17a42755c46df66340
System Administrator <zaagagon@gmail.com>
2022-03-30 23:58:45 -0500

    routes app get

--- expreso.js
+++ expreso.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) => {
   res.send('<h1 style= "color: blue">Hello zag!</h1>')//color tag //
 })
 
+app.get('/nosotros', (req, res) => {
+  // res.write('<h1>Express funcionando</h1>');
+  res.writeHead(200,{'Content-type':'text/html'});//para que funcione necesita
+   res.write('<h1>Pagina principal acerca de nosotros</h1>');
+ res.send('<h1 style= "color: blue">Hello zag!</h1>')//color tag //
+})
+
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`)
 })
