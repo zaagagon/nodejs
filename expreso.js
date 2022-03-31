a56bd75ba69b27f29e1d7ac79c54f80ee49a3592
System Administrator <zaagagon@gmail.com>
2022-03-30 23:57:26 -0500

    revisino

--- expreso.js
+++ expreso.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
    // res.write('<h1>Express funcionando</h1>');
    res.writeHead(200,{'Content-type':'text/html'});//para que funcione necesita
     res.write('<h1>Pagina principal ADMIN</h1>');
-  res.send('<h1 style= "color: blue">Hello zag!</h1>')//color tag
+  res.send('<h1 style= "color: blue">Hello zag!</h1>')//color tag //
 })
 
 app.listen(port, () => {
