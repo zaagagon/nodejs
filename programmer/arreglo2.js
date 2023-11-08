b73d8138ec00809ac3ec8534af3733bcf5799105
zaagagon <zaagagon@gmail.com>
2023-11-07 23:43:27 -0500

    foreach en arreglo2

--- programmer/arreglo2.js
+++ programmer/arreglo2.js
@@ -5,3 +5,7 @@ console.log("prueba git");
 
 const arreglo=[9,8,"marvel"]
 console.log(arreglo);
+console.log("recorrido del arreglo : ");
+arreglo.forEach(element => {
+    console.log(element);
+});
