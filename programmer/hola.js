c400bb25c9a1cb8610f9c5c5b784758abb144aaa
zaagagon <zaagagon@gmail.com>
2024-02-06 19:56:09 -0500

    practicando javascript

--- programmer/hola.js
+++ programmer/hola.js
@@ -1,5 +1,5 @@
 //bot que saluda
 //variable creada e inicializada
-let nombre ="kevin"
-alert("hola " + nombre)
+let nombre ="Miguel"
+//alert("hola " + nombre)
 console.log(nombre);
---
+++ programmer/hola3.js
@@ -0,0 +1,6 @@
+//mi primer programa en javascritp
+
+//instruccion que guarda en una variable
+let ciudad = " medellin" //variable ciudad creada
+
+console.log("Que bueno que Migue vive en : " + ciudad);
